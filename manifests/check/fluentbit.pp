5be1bb73d5e2827ff0de5679b3ca01fab170229d
Juan Pablo Firrincieli <juanpablo.firrincieli@exoclick.com>
2024-04-19 11:06:08 +0200

    Fixed fluentbit check to use getopts

--- manifests/check/fluentbit.pp
+++ manifests/check/fluentbit.pp
@@ -21,24 +21,25 @@ class nagios::check::fluentbit (
   $fullargs = strip("${arg_h}${arg_p}${args}")
 
   # Let's check if the fluentbit check script is present and if not copy it
-  file { '/usr/lib64/nagios/plugins/check_fluentbit_health.sh':
+  file { '/usr/lib64/nagios/plugins/check_fluentbit_health':
     ensure => $ensure,
     owner  => 'root',
     group  => 'root',
     mode   => '0755',
-    source => "puppet:///modules/${module_name}/scripts/check_fluentbit_health.sh",
+    source => "puppet:///modules/${module_name}/scripts/check_fluentbit_health",
   }
 
   nagios::client::nrpe_file { 'check_fluentbit_health':
-    ensure => $ensure,
-    args   => $fullargs,
-    command => '/usr/lib64/nagios/plugins/check_fluentbit_health.sh',
+    ensure  => $ensure,
+    args    => $fullargs,
+    require => File['/usr/lib64/nagios/plugins/check_fluentbit_health'],
+    plugin  => 'check_fluentbit_health',
   }
 
   nagios::service { "check_fluentbit_health_${check_title}":
     ensure                   => $ensure,
-    check_command            => 'check_nrpe!check_fluentbit_health',
-    service_description      => 'Fluent Bit Health Check',
+    check_command            => 'check_nrpe_fluentbit_health',
+    service_description      => 'fluentbit_health',
     servicegroups            => $servicegroups,
     check_period             => $check_period,
     contact_groups           => $contact_groups,
